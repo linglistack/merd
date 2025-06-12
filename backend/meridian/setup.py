d7f191b7853b6c8738e7a9cf545a594b027cc384
Gantz <lli12@syr.edu>
2025-06-12 08:08:21 -0700

    f

--- backend/meridian/setup.py
+++ backend/meridian/setup.py
@@ -5,14 +5,14 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy>=1.23.5",
-        "pandas>=1.5.3",
-        "tensorflow-cpu>=2.10.1",
-        "tensorflow-probability>=0.18.0",
-        "scipy>=1.9.3",
+        "numpy>=1.24.0",
+        "pandas>=2.1.1",
+        "tensorflow-cpu>=2.13.0",
+        "tensorflow-probability>=0.21.0",
+        "scipy>=1.11.0",
         "xarray>=2023.1.0",
-        "h5py>=3.8.0",
-        "arviz>=0.14.0"
+        "h5py>=3.9.0",
+        "arviz>=0.15.0"
     ],
     python_requires=">=3.10",
 )
